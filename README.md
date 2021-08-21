1d8fc777b165724d1fb1ed4eddb7e31eaccc23df
Viyzed <1407637@rgu.ac.uk>
2021-08-21 11:40:36 +0000

    Add comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # masters-ids-monitor
 Monitor of network and application logs to pass to ids instance.
+
+All Pcaps are uploaded to S3 storage to be downloaded by ids.
--- main.py
+++ main.py
@@ -8,25 +8,32 @@ from botocore.exceptions import ClientError
 
 class PutLogs:
     
+    # global varibales for log file filenames and S3 bucket name and path
     global filename, timestamp, object_name, bucket_name
     filename = ''
     timestamp = ''
     object_name = 'instance-logs/'
     bucket_name = 'masters-lb-access-logs'
         
+    # scapy sniff for TCP packets on the enthernet interface
     def capturePackets(seconds):
         s3_client = boto3.client('s3')
 
+        # compile filename to save the log file as 
         timestamp = datetime.datetime.now()
         timestamp = timestamp.strftime('%m') + '-' + timestamp.strftime('%d') + '-' + timestamp.strftime('%y') + '-' + timestamp.strftime('%X')
         global filename
         filename = str(timestamp+'_capture.pcap')
 
+        # capture packets
         capture = sniff(filter='tcp', iface="eth0", timeout=seconds)
         wrpcap("pcaps/"+filename, capture)
 
+    # upload papcket captures as .pcap to S3 bucket 
     def uploadPackets():
         s3_client = boto3.client('s3')
+        
+        # try/catch to upload .pcap file to S3 bucket and delete .pcap file from localhost
         try:
             response = s3_client.upload_file('pcaps/'+filename, bucket_name, object_name+filename)
             os.remove("pcaps/"+filename)
@@ -37,6 +44,7 @@ class PutLogs:
     
     if __name__ == "__main__":
         while True:
+            # run capture and upload every five seconds
             capturePackets(5)
             uploadPackets()
     
